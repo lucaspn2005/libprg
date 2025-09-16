8431dd13cd33188cf6d91bc52c2ba9ce4c068898
lucas.pn2005 <lucas.pn2005@aluno.ifsc.edu.br>
2025-09-16 11:24:54 -0300

    Lista

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -1,11 +1,7 @@
 #ifndef LIBPRG_LIBPRG_H
 #define LIBPRG_LIBPRG_H
-
-typedef struct pilha {
-    int* elementos;
-    int topo;
-    int capacidade;
-} pilha_t;
+// ----------PILHA-----------------
+typedef struct pilha pilha_t;
 pilha_t* criar_pilha(int capacidade);
 int empilhar(pilha_t* p, int valor);
 int desempilhar(pilha_t* p, int* valor);
@@ -14,21 +10,21 @@ int tamanho_pilha(pilha_t* p);
 void imprimir_pilha(pilha_t* p);
 void destruir_pilha(pilha_t* p);
 
-typedef struct fila {
-    int* elementos;
-    int inicio;
-    int fim;
-    int tamanho;
-    int capacidade;
-}fila_t;
+// -------- FILA------------
 
+typedef struct fila  fila_t;
 fila_t* criar_fila(int capacidade) {
     fila_t* f = malloc(sizeof(fila_t));
 
     f->elementos= malloc(capacidade*sizeof(int));
     f->inicio=0;
-
-
 }
+
+// -----------LISTA--------------
+typedef struct  lista_linear lista_linear_t;
+lista_linear_t* criar (int capacidade);
+void inserir (lista_linear_t* lista, int valor);
+bool lista_cheia(lista_linear_t* lista);
+int buscar (lista_linear_t* lista, int valor;
 
 #endif
--- libprg/src/libprg/fila.c
+++ libprg/src/libprg/fila.c
@@ -1,6 +1,12 @@
 #include "libprg/libprg.h"
 
-
+typedef struct fila {
+    int* elementos;
+    int inicio;
+    int fim;
+    int tamanho;
+    int capacidade;
+}fila_t;
 
 
 // criar_fila
